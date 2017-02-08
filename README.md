589d45da01b9481dce99ad0c4a396123dffec92e
FoxMaSk <foxmaskhome@gmail.com>
2017-02-08 14:48:11 +0100

    details and testing

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Orotangi
 Your thoughts everywhere 
 
 
+Installation
+------------
+
+```bash
+virtualenv orotangi
+cd $_
+git clone http://github.com/foxmask/orotangi
+pip install -r requirements.txt
+cd orotangi
+./manage.py migrate
+./manage.py runserver localhost:8000
+```
--- orotangi/api/serializers.py
+++ orotangi/api/serializers.py
@@ -19,5 +19,4 @@ class NoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Notes
-        fields = ('user', 'book', 'tags', 'url', 'title', 'content',
-                  'date_created', 'date_modified', 'date_deleted', 'status')
+        fields = ('__all__')
