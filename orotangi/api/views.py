196f58c4cb1079c656b64428ea41a51101d18e38
FoxMaSk <foxmaskhome@gmail.com>
2017-02-23 23:50:04 +0100

    get only 'my' stuff

--- orotangi/api/views.py
+++ orotangi/api/views.py
@@ -1,7 +1,20 @@
+from django.http import HttpResponse
 from orotangi.api.serializers import NoteSerializer
 from orotangi.api.serializers import BookSerializer, TagSerializer
 from orotangi.models import Notes, Books, Tags
+from rest_framework.renderers import JSONRenderer
 from rest_framework import viewsets
+
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
 
 class NoteViewSet(viewsets.ModelViewSet):
     """
@@ -10,6 +23,19 @@ class NoteViewSet(viewsets.ModelViewSet):
     """
     queryset = Notes.objects.all()
     serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        return Notes.objects.filter(user=self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        data = request.data
+        data['user'] = request.user.id
+        instance = self.get_object()
+        serializer = NoteSerializer(instance, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -20,6 +46,9 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BookSerializer
 
+    def get_queryset(self):
+        return Books.objects.filter(user=self.request.user)
+
 
 class TagViewSet(viewsets.ModelViewSet):
     """
@@ -28,3 +57,6 @@ class TagViewSet(viewsets.ModelViewSet):
     """
     queryset = Tags.objects.all()
     serializer_class = TagSerializer
+
+    def get_queryset(self):
+        return Tags.objects.filter(user=self.request.user)
