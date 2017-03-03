f8a36e4b2d61c029def5624781d7f55e0f2904eb
FoxMaSk <foxmaskhome@gmail.com>
2017-03-03 23:25:03 +0100

    handle tags

--- orotangi/api/views.py
+++ orotangi/api/views.py
@@ -4,6 +4,8 @@ from orotangi.api.serializers import BookSerializer, TagSerializer
 from orotangi.models import Notes, Books, Tags
 from rest_framework.renderers import JSONRenderer
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class JSONResponse(HttpResponse):
@@ -24,16 +26,43 @@ class NoteViewSet(viewsets.ModelViewSet):
     queryset = Notes.objects.all()
     serializer_class = NoteSerializer
 
+    def update_tags(self, tags):
+        my_tags = []
+        for tag in tags:
+            obj, created = Tags.objects.get_or_create(tag=tag.strip(),
+                                                      user=self.request.user,
+                                                      defaults={'tag': tag.strip(),
+                                                                'user': self.request.user})
+            if created:
+                my_tags.append(created)
+            else:
+                my_tags.append(obj)
+        return my_tags
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        data['user'] = request.user.id
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def get_queryset(self):
         return Notes.objects.filter(user=self.request.user)
 
     def partial_update(self, request, *args, **kwargs):
         data = request.data
+        if len(data['tags']) > 0:
+            tags = self.update_tags(data['tags'])
         data['user'] = request.user.id
+        data['tags'] = tags
         instance = self.get_object()
         serializer = NoteSerializer(instance, data=data, partial=True)
+
         if serializer.is_valid():
             serializer.save()
+            print(serializer.data)
             return JSONResponse(serializer.data)
         return JSONResponse(serializer.errors, status=400)
 
